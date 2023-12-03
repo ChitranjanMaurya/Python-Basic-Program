58f7693091b19865fc8d4d45152f8de790307473
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-03 16:07:45 +0530

    Update basic.py

---
+++ Basic/basic.py
@@ -0,0 +1,28 @@
+# This is comment started with "#"
+
+# Using print()
+print("Hello World")
+print(123)
+#Output: Hello World
+#Output: 123
+
+# Using Type()
+A = 10
+print(A)
+print(type(A))
+b = "Python"
+print(type(b))
+c = 10.5
+print(type(c))
+#Output: <class 'int'>
+#Output: <class 'str'>
+#Output :<class 'float'>
+
+# Using input()
+a = input("Enter the value of A :")
+print("A : ",a)
+#Output: Enter the value of A :
+#Input:  10
+#Output: A : 10
+
+# To apply multilines comments use this shortcut keys == ctrl+ question marks
