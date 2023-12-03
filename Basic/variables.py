f0f5898bf9bf5d4dbea7603affc0c18d4876cee1
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-03 17:59:44 +0530

    Update Basic

---
+++ Basic/variables.py
@@ -0,0 +1,35 @@
+#Creating a variable
+# integer value to the variable
+a = 10
+print(a)
+print(type(a))
+#Output: 10
+#Output: <class 'int'>
+
+# Float value to the variable
+b = 10.4
+print(b)
+print(type(b))
+#Output: 10.4
+#Output: <class 'float'>
+
+# String value to the variable
+c = "Python"
+print(c)
+print(type(c))
+#Output: Python
+#Output: <class 'str'>
+
+# List store in variable
+l1 = [1,2,3,4,5,6]
+print(l1)
+print(type(l1))
+#Output: [1, 2, 3, 4, 5, 6]
+#Output: <class 'list'>
+
+# Tuple store in variable
+t1 = (1,2,3,4,5)
+print(t1)
+print(type(t1))
+#Output: (1, 2, 3, 4, 5)
+#Output: <class 'tuple'>
