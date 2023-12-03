c19d2c82d2255547bb8c0bda3252946bdb180c71
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-03 18:13:02 +0530

    Update Basic

---
+++ Basic/datatypes.py
@@ -0,0 +1,50 @@
+#1.Numeric Datatype
+  #Int Datatype
+  a = 10
+  print(type(a))
+
+  # Float Datatype
+  b = 10.5
+  print(type(b))
+
+  # Complex Datatype
+  c = 2+4j
+  print(type(c))
+
+<class 'int'>
+<class 'float'>
+<class 'complex'>
+
+#2.Boolean Datatype
+d = True
+print(type(d))
+e = False
+print(type(e))
+<class 'bool'>
+<class 'bool'>
+
+#3.Set Datatype
+f = {'a','b''c'}
+print(type(f))
+<class 'set'>
+
+#4.Dictionary Datatype
+x = {"Key":"Value"}
+print(type(x))
+<class 'dict'>
+
+#5.Sequence Datatype
+  # List Datatype
+  l1 = [1,2,3,4,5]
+  print(type(l1))
+
+  # String Datatype
+  y = "Python Program"
+  print(type(y))
+
+  # Tuple Datatype
+  z = (1,2,3,4,5)
+  print(type(z))
+  <class 'list'>
+  <class 'str'>
+  <class 'tuple'>
