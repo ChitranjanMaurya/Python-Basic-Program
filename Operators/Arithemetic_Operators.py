ed772e8bc5e8e980a34aeba186f2411ec83eabfe
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-04 13:25:07 +0530

    Update Operators

---
+++ Operators/Arithemetic_Operators.py
@@ -0,0 +1,20 @@
+# Addition operator
+x = 10
+y = 20
+z = x+y
+print("Answer : ",z)
+#Output: Answer : 30
+
+# Subtraction Operator
+x = 30
+y = 10
+z = x - y
+print("Answer : ",z)
+#Output: Answer : 20
+
+# Multiplication Operator
+x = 10
+y = 2
+z = x*y
+print("Answer : ",z)
+#Output: Answer : 20
