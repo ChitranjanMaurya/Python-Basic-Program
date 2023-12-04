b9380edbe12852b9401f1c2a3855fe72ff438817
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-04 13:27:08 +0530

    Update Operators

---
+++ Operators/Assignment_Operator.py
@@ -0,0 +1,17 @@
+# Equalto operator
+x = 5
+print("X : ",x)
+#Output: X : 5
+
+# Addition and Equalto Operator
+x = 5
+x+=5  #x = x+5
+print("X : ",x)
+#Output: X : 10
+
+
+# Subtraction and Equalto operator
+x = 6
+x-=2  #x = x-2
+print("X : ",x)
+#Output: X : 4
