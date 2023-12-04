d1b532d2a3e37bef56cb25e52d9553f24a85d112
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-04 13:28:49 +0530

    Update Operators

---
+++ Operators/Comparsion_Operator.py
@@ -0,0 +1,17 @@
+# Equalto-Equalto Operator
+x = 5
+y = 5
+print(x==y)
+#Output: True
+
+# not Equalto Operator
+x = 5
+y = 3
+print(x!=y)
+#Output: True
+
+# Greater than equalto
+x = 5
+y = 3
+print(x>=y)
+#Output: True
