d689cc2f842bb9eaf906ec62ca980905cb908a3e
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-04 13:35:55 +0530

    Update Operators

---
+++ Operators/Operator_Precedence.py
@@ -0,0 +1,17 @@
+a = 20
+b = 10
+c = 15
+d = 5
+
+x = (a+b)*c/d #(30*15)/5
+print("Value : ",x)
+
+y = ((a+b)*c)/d # (30*15)/5
+print("Value of Y : ",y)
+
+z = a+(b*c)/d # 20+(150/5)
+print("Value of Z : ",z)
+
+#Output: Value : 90.0
+#Output: Value of Y : 90.0
+#Output: Value of Z : 50.0
