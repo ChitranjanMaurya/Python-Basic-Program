e2ea33260575d749ce71a46d50d11e5ea74216d6
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-04 13:32:17 +0530

    Update Operators

---
+++ Operators/identity_Operator.py
@@ -0,0 +1,12 @@
+# Is operator
+x = ["Apple","Mango"]
+y = ["Apple","Mango"]
+z = x
+print(x is y) 
+#the object is not matching that's why it give as False
+
+print(x is z) 
+#the object is matching that' why it returns True
+
+#Output: False
+#Output: True
