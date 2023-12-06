d4ea8ea697929d6624fc7081755c20cc7a1ff523
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-06 19:45:24 +0530

    Update simple_if.py

--- Conditional Statements/simple_if.py
+++ Conditional Statements/simple_if.py
@@ -1,12 +1,12 @@
 var = 10
-if(var==10): # your condition is getting True
+if(var==10): #condition is getting True
  print("Value of variable is 10")
  print("Bye")
 #Output: Value of variable is 10
 #Output: Bye
 
 a = 25
-if(a>=30): # Condition is getting False
+if(a>=30): #Condition is getting False
  print("Value of variable is 25")
  print("bye")
 #Output: bye
