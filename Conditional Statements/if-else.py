7d4d6590c769032a94379877f1723e3f1eeefd93
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-06 19:49:53 +0530

    Update Conditional Statements

---
+++ Conditional Statements/if-else.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the value of A : "))
+b = int(input("Enter the value of B : "))
+if a<b:  #Condition is getting True
+ print("B is greater")
+else:  #Else part call
+ print("A is greater")
+#Input: Enter the value of A :10
+#Input: Enter the value of A :20
+#Output: B is greater
+
+x = 25
+y = 30
+if x<y: # Condition True
+ print("Y is greater")
+else:
+ print("X is greater")
+ #Output: Y is greater
