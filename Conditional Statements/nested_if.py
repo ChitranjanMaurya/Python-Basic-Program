39f5c8ed4e734449b50f7ecc0e99e7ca5d1e313e
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-06 20:29:55 +0530

    Update Conditional Statements

---
+++ Conditional Statements/nested_if.py
@@ -0,0 +1,18 @@
+#1)Age>=18
+#2)Weight>=50
+#3)if both age and weight match with certeria then the user can donate the blood
+#4)if age is less than 18 show one msg under age
+#5)if weight is less than 50 show one msg under weight
+#6)if age doesnot match then it should ask for weight
+age = int(input("Enter your Age : "))
+if age>=18:
+ weg = int(input("Enter your Weight : "))
+ if weg>=50:
+  print("Donate Blood")
+ else:
+  print("Under Weight")
+else:
+  print("Under Age")
+#Input: Enter your Age : 19
+#Input: Enter your Weight : 65
+#Output: Donate Blood
