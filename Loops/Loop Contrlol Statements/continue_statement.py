55cef806851b4e9beeb248e23f673fb47842db9b
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-08 16:50:35 +0530

    Update continue_statement.py

---
+++ Loops/Loop Contrlol Statements/continue_statement.py
@@ -0,0 +1,12 @@
+for letter in "Python":
+ if letter == 'h':
+#Continue statement will skip the "h"
+  continue
+ print("Current Letter :",letter)
+print("bye")
+#Output: Current Letter : P
+#        Current Letter : y
+#        Current Letter : t
+#        Current Letter : o
+#        Current Letter : n
+#        bye
