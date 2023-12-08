84c27a0af3004c3a6aadf5ec6b4508bf33662f31
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-08 17:07:17 +0530

    Update string.py

--- Basic/Datatypes Collections/Sequence/string.py
+++ Basic/Datatypes Collections/Sequence/string.py
@@ -15,3 +15,10 @@ print(a)
 #Output: Python is booming in the Market,
 #Output: Python is smart language,
 #Output: Python is used in AI,Machine Learning,Data Science
+
+#Update String
+a = "Hello World!"
+print(a)
+print("Update String : ",a[:6]+"Python")
+#Output: Hello World!
+#Output: Update String : Hello Python
