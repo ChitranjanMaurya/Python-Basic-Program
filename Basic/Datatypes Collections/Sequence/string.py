d0b3460a98dd38aba91eff26e493209c543c2b82
Chitranjan Maurya <140469446+ChitranjanMaurya@users.noreply.github.com>
2023-12-08 17:09:53 +0530

    Update string.py

--- Basic/Datatypes Collections/Sequence/string.py
+++ Basic/Datatypes Collections/Sequence/string.py
@@ -22,3 +22,18 @@ print(a)
 print("Update String : ",a[:6]+"Python")
 #Output: Hello World!
 #Output: Update String : Hello Python
+
+#String Slicing
+a = "Hello Python"
+#Access the value of Index 1
+print("Index 1 : ",a[1])
+#Access the value Between index 1 to 6
+print("Index between 1 to 6 : ",a[1:6])
+#Access the value till index 6
+print("Index till 6 : ",a[:7])
+#Access the last value from String
+print("Last Value : ",a[-1])
+#Output: Index 1 : e
+#Output: Index between 1 to 6 : ello 
+#Output: Index till 6 : Hello P
+#Output: Last Value : n
