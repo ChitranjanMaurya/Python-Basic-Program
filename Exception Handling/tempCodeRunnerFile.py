fc9a3b469c06a2309d385bcff5e134ffdf44b79a
Bhutoo <bhutoo@gmail.com>
2023-12-16 21:37:10 +0530

    update

--- Exception Handling/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-----------------------------------------------------------------------
-# -----
-# MyException Traceback (most recent call 
-# last)
-# <ipython-input-19-a78115dfe7c2> in <module>
-#  4 c = 25
-#  5 if c>5:
-# ----> 6 raise MyException("Something went worng")
-# MyException: Something went worng
